1da1c1107095a7d81113c42df3f41479e807c174
Eruchan <candystick93-fun@yahoo.de>
2015-07-17 08:23:11 +0200

    First

---
+++ cherry wars/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour {
+
+
+	public Button startGame;
+	public Button exitGame;
+
+	// Use this for initialization
+	void Start () {
+
+		startGame = startGame.GetComponent<Button>();
+		exitGame = exitGame.GetComponent<Button>();
+
+	
+	}
+
+
+	public void exitpress()
+	{
+		Application.Quit();
+	}
+
+	public void startpress()
+	{
+		Application.LoadLevel(1);
+	}
+
+	
+}
